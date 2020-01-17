1fce8c8bd0f73aa374078b5b35780734f740d095
Jesús García <jesus.garcia@habitant.es>
2020-01-17 14:02:52 +0100

    Volvemos a JSX.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to React</h1>
-    //     </header>
-    //       <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
-    return React.createElement('div', {className: 'App'},
-        React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+          <h1>Hi, I'm a React App</h1>
+      </div>
+    );
   }
 }
 
