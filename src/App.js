b9f9d9eb735c1d510f6c0e0c9897c52c4700a27e
Jesús García <jesus.garcia@habitant.es>
2020-01-21 18:42:22 +0100

    Revert "Pasamos el componente a su versión "functional" y manipulamos el state mediante el uso del Hook "useState()"."
    This reverts commit e0b35544: recuperamos el componente en su versión "class-based".

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
     otherState: 'Cocococococo'
-  });
+  };
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  // Como no es una clase, sino una función, no podemos usarlo sin variable
-  // También estamos definiendo una función dentro de otra función
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     console.log('Was clicked!');
     // No hacer esto
-    // personsState.persons[0].name = 'Maximilian';
-    setPersonsState({
+    // this.state.persons[0].name = 'Maximilian';
+    this.setState( {
       persons: [ // Sobreescribe persons ya que lo estamos definiendo de nuevo por completo
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
@@ -32,19 +26,21 @@ const App = props => {
     })
   };
 
-  return (
+  render() {
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <h1>Hi, I'm a React App</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+          <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
