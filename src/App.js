82bdcd953d48878980b74df7969d2a90f0591e55
Jesús García <jesus.garcia@habitant.es>
2020-01-22 13:51:01 +0100

    Estilos en línea.

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ class App extends Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <header className="App-header">
@@ -44,7 +52,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
           <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNameHandler('Maximilian!!') }>Switch Name</button>
+        <button
+            style={style}
+            onClick={() => this.switchNameHandler('Maximilian!!') }>Switch Name</button>
         <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
