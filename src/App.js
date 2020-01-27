43a5ded9aa8fe39c642ff1055cc0f985689f249b
Jesús García <jesus.garcia@habitant.es>
2020-01-27 18:30:54 +0100

    Eliminamos personas de nuestra lista a través de un handler y actualizando nuestro state.

--- src/App.js
+++ src/App.js
@@ -30,6 +30,15 @@ class App extends Component {
     })
   };
 
+  deletePersonHandler = (personIndex) => {
+    // Fetch all Persons
+    const persons = this.state.persons;
+    // Quitamos el elemento en index
+    persons.splice(personIndex, 1);
+    // Actualizamos el state
+    this.setState({persons: persons});
+  };
+
   nameChangeHandler = (event) => {
     this.setState( {
       persons: [ // Sobreescribe persons ya que lo estamos definiendo de nuevo por completo
@@ -65,23 +74,24 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
           <div>
-            <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-            />
-            <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                changed={this.nameChangeHandler}>
-              My Hobbies: Racing
-            </Person>
-            <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-            />
-            {this.state.persons.map(person => {
+            {/*<Person*/}
+            {/*    name={this.state.persons[0].name}*/}
+            {/*    age={this.state.persons[0].age}*/}
+            {/*/>*/}
+            {/*<Person*/}
+            {/*    name={this.state.persons[1].name}*/}
+            {/*    age={this.state.persons[1].age}*/}
+            {/*    changed={this.nameChangeHandler}>*/}
+            {/*  My Hobbies: Racing*/}
+            {/*</Person>*/}
+            {/*<Person*/}
+            {/*    name={this.state.persons[2].name}*/}
+            {/*    age={this.state.persons[2].age}*/}
+            {/*    click={this.switchNameHandler.bind(this, 'Max!')}*/}
+            {/*/>*/}
+            {this.state.persons.map((person, index) => {
               return <Person
+                  click={() => this.deletePersonHandler(index)}
                   name={person.name}
                   age={person.age} />
             })}
