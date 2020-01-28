81aa514539c87ec4b6d4c42a56eff62980cffdf5
Jesús García <jesus.garcia@habitant.es>
2020-01-28 18:43:04 +0100

    Gestión flexible de listas mediante la inmutabilidad del state, objetos, etc.

--- src/App.js
+++ src/App.js
@@ -39,14 +39,24 @@ class App extends Component {
     this.setState({persons: persons});
   };
 
-  nameChangeHandler = (event) => {
-    this.setState( {
-      persons: [ // Sobreescribe persons ya que lo estamos definiendo de nuevo por completo
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // Alternative version
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons});
   };
 
   nameStateChangeHandler = (event) => {
@@ -92,6 +102,7 @@ class App extends Component {
             {this.state.persons.map((person, index) => {
               return <Person
                   click={() => this.deletePersonHandler(index)}
+                  changed={(event) => this.nameChangeHandler(event, person.id)}
                   key={person.id}
                   name={person.name}
                   age={person.age} />
